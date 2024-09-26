3604f5e3336f0bcb21543c594b40ffd4109de296
apepeoglu <alara.pepeoglu@smail.th-koeln.de>
2024-09-26 13:25:14 +0200

    [TASK] F5b: Added finished work dynamically

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,11 @@
+import { initworkList } from './modules/generateContent.js';
 import { slideshow } from './modules/slideshow.js';
 
 /* Main
 ############################################################################ */
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   hljs.highlightAll();
+  initworkList();
   slideshow();
 });
---
+++ assets/js/modules/generateContent.js
@@ -0,0 +1,52 @@
+const workJSON = 'http://localhost:8080/work.json';
+let workList = null;
+let currentData = null;
+
+document.addEventListener('DOMContentLoaded', function () {
+    initworkList();
+});
+
+function initworkList() {
+    workList = document.querySelector('[data-js-finished-works]');
+
+    if (!workList) {
+        console.error('Element with data-js-finished-works not found.');
+        return;
+    }
+
+    fetch(workJSON)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json()
+        })
+
+        .then(data => {
+            console.log('Success:', data);
+            currentData = data;
+            generateMarkupForworkList(data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+
+function generateMarkupForworkList(data) {
+    workList.innerHTML = '';
+    workList.innerHTML = data.map(work => {
+        return `
+                    <li class="topic-item">
+          <a class="wrap" href="${work.url}">
+            <h3>${work.title}</h3>
+            <p>${work.date}</p>
+            <ul class="keywords">
+              ${work.keywords}
+            </ul>
+          </a>
+        </li>
+        `;
+    }).join('');
+}
+
+export { initworkList };
